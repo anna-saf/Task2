92eb4b291e6ce73881ebb48a4191fdeb01f5905c
anna-saf <safonova.irage@yandex.ru>
2022-07-22 23:43:55 +0300

    Final
    Task2

---
+++ Task2/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{  
+
+    class Car
+    {
+        public string Model { get; set; }
+        protected string Color { get; set; }
+        protected string Description { get; set; }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Model, Color);
+        }
+
+        public bool Equals(Car car)
+        {
+            return car!=null && Model.Equals(car.Model) && Color.Equals(car.Color);
+        }
+    }
+
+    
+}
